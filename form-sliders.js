1965eb9b94477797554f454650df2589365e285a
darrylkelly88 <88737523+darrylkelly88@users.noreply.github.com>
2023-09-07 13:44:43 +0100

    commit

---
+++ form-sliders.js
@@ -0,0 +1,27 @@
+// Update text input for "How many cores?" slider
+coresSlider.addEventListener('input', function () {
+    coresInput.value = coresSlider.value;
+});
+
+// Update text input for "How many VM's?" slider
+vmsSlider.addEventListener('input', function () {
+    vmsInput.value = vmsSlider.value;
+});
+
+// Update text input for "How many Socket Pairs?" slider
+socketPairsSlider.addEventListener('input', function () {
+    socketPairsInput.value = socketPairsSlider.value;
+});
+
+// Update text input for "How many LPAR's?" slider
+lparsSlider.addEventListener('input', function () {
+    lparsInput.value = lparsSlider.value;
+});
+
+// Initial updates when the page loads
+window.onload = function() {
+    coresInput.value = coresSlider.value;
+    vmsInput.value = vmsSlider.value;
+    socketPairsInput.value = socketPairsSlider.value;
+    lparsInput.value = lparsSlider.value;
+};
