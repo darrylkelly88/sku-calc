365afd802a2cbed4f20ea9821713050b0ad01048
darrylkelly88 <darrylkelly88@gmail.com>
2023-10-16 15:19:42 +0100

    update

--- rhel-sku-filter.js
+++ rhel-sku-filter.js
@@ -17,29 +17,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 const pctype = document.getElementById("pctype").value;
                 const SAPversion = document.getElementById("SAPversion").value
 
-                // Filter the JSON data based on user selections
-                const filteredData = data.filter((item) => {
-                    // Determine the SKU field to display based on the selected term
+                if (product === "Red Hat Enterprise Linux (RHEL)") {
+                    // Filter the JSON data based on user selections
+                    const filteredData = data.filter((item) => {
+                        // Determine the SKU field to display based on the selected term
 
-                    // Check if the user made a selection in each field before applying the filter
-                    return (
-                        (!runSAP || (runSAP === "Yes" && item["SAP"] === "TRUE") || (runSAP === "No" && item["SAP"] === "FALSE")) &&
-                        (!SAPversion || (SAPversion === "minimum" && item["Sap Applications"] === "TRUE") || (SAPversion === "solution" && item["SAP Solutions"] === "TRUE")) &&
-                        (!pctype || (pctype === "workstation" && item["Workstation"] === "TRUE") || (pctype === "HPC" && item["HPC"] === "TRUE" || (pctype === "server" && item["Server"] === "TRUE"))) &&
-                        (!architecture || (architecture === "x86" && item["x86"] === "TRUE") || (architecture === "IBM POWER" && item["IBM POWER"] === "TRUE") || (architecture === "ARM" && item["ARM"] === "TRUE")) &&
-                        (!virtualOrBareMetal || (virtualOrBareMetal === "Virtual" && item["Virtual"] === "TRUE") || (virtualOrBareMetal === "Bare Metal" && item["Physical"] === "TRUE")) &&
-                        (!densityGreaterThan7 || (densityGreaterThan7 === "Yes" && item["Virtual Datacenters"] === "TRUE") || (densityGreaterThan7 === "No" && item["Virtual Datacenters"] === "FALSE")) &&
-                        (!satelliteAddon || (satelliteAddon === "Yes" && item["Satellite / Smart Management"] === "TRUE") || (satelliteAddon === "No" && item["Satellite / Smart Management"] === "FALSE")) &&
-                        (!supportLevel || (supportLevel === "standard" && item["Standard"] === "TRUE") || (supportLevel === "premium" && item["Premium"] === "TRUE")) &&
-                        // standard exclusion filters
-                        (item["Add-on"] !== "TRUE") && 
-                        (item["Is it for Edge, Endpoint or gateway?"] !== "TRUE") &&
-                        (item["Include in Data?"] == "TRUE") 
-                   );
-                });
+                        // Check if the user made a selection in each field before applying the filter
+                        return (
+                            (!runSAP || (runSAP === "Yes" && item["SAP"] === "TRUE") || (runSAP === "No" && item["SAP"] === "FALSE")) &&
+                            (!SAPversion || (SAPversion === "minimum" && item["Sap Applications"] === "TRUE") || (SAPversion === "solution" && item["SAP Solutions"] === "TRUE")) &&
+                            (!pctype || (pctype === "workstation" && item["Workstation"] === "TRUE") || (pctype === "HPC" && item["HPC"] === "TRUE" || (pctype === "server" && item["Server"] === "TRUE"))) &&
+                            (!architecture || (architecture === "x86" && item["x86"] === "TRUE") || (architecture === "IBM POWER" && item["IBM POWER"] === "TRUE") || (architecture === "ARM" && item["ARM"] === "TRUE")) &&
+                            (!virtualOrBareMetal || (virtualOrBareMetal === "Virtual" && item["Virtual"] === "TRUE") || (virtualOrBareMetal === "Bare Metal" && item["Physical"] === "TRUE")) &&
+                            (!densityGreaterThan7 || (densityGreaterThan7 === "Yes" && item["Virtual Datacenters"] === "TRUE") || (densityGreaterThan7 === "No" && item["Virtual Datacenters"] === "FALSE")) &&
+                            (!satelliteAddon || (satelliteAddon === "Yes" && item["Satellite / Smart Management"] === "TRUE") || (satelliteAddon === "No" && item["Satellite / Smart Management"] === "FALSE")) &&
+                            (!supportLevel || (supportLevel === "standard" && item["Standard"] === "TRUE") || (supportLevel === "premium" && item["Premium"] === "TRUE")) &&
+                            // standard exclusion filters
+                            (item["Add-on"] !== "TRUE") && 
+                            (item["Is it for Edge, Endpoint or gateway?"] !== "TRUE") &&
+                            (item["Include in Data?"] == "TRUE") 
+                       );
+                    });
 
-                // Display the filtered results in the "skuList" div
-                displayFilteredResults(filteredData);
+                    // Display the filtered results in the "skuList" div
+                    displayFilteredResults(filteredData);
+                }
             });
         })
         .catch((error) => {
